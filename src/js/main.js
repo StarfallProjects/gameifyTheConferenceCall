f09b7371364836ac5fe9f04c1dd7a5d5daa07f0d
scribblerp9 <szappia86@gmail.com>
2021-03-25 15:22:09 +1100

    Access database differently
    Tried using Supabase's module to access the data rather than a standard API call

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
+const { createClient } = supabase
+supabase = createClient("https://nubzowhjmtpqanaaanfv.supabase.co", 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIzMzA5NywiZXhwIjoxOTMwODA5MDk3fQ.SmwcEgVk3ZTQEVntPT67se3GKio3OCGwLnUh_ZdY6qU');
+
 function getGameData() {
   return {
     init() {
-      getData('https://nubzowhjmtpqanaaanfv.supabase.co/rest/v1/test?select=*')
-      .then((data) => {
+      getData('test')
+      .then((response) => {
         // Log the raw data response for reference
-        console.log('raw data:',data);
+        console.log('raw data:',response);
         
         // Set component values
-        this.title = data[0].gamename;
+        this.title = response.data[0].gamename;
         
         // Update page title
         document.title = this.title;
@@ -17,22 +20,28 @@ function getGameData() {
   }
 }
 
-async function getData(url = '',) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIzMzA5NywiZXhwIjoxOTMwODA5MDk3fQ.SmwcEgVk3ZTQEVntPT67se3GKio3OCGwLnUh_ZdY6qU',
-      'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIzMzA5NywiZXhwIjoxOTMwODA5MDk3fQ.SmwcEgVk3ZTQEVntPT67se3GKio3OCGwLnUh_ZdY6qU'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    //body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
+async function getData(tableName) {
+    return await supabase
+    .from(tableName)
+    .select('*')
 }
+
+// async function getData(url = '',) {
+//   // Default options are marked with *
+//   const response = await fetch(url, {
+//     method: 'GET', // *GET, POST, PUT, DELETE, etc.
+//     mode: 'cors', // no-cors, *cors, same-origin
+//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: 'same-origin', // include, *same-origin, omit
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIzMzA5NywiZXhwIjoxOTMwODA5MDk3fQ.SmwcEgVk3ZTQEVntPT67se3GKio3OCGwLnUh_ZdY6qU',
+//       'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIzMzA5NywiZXhwIjoxOTMwODA5MDk3fQ.SmwcEgVk3ZTQEVntPT67se3GKio3OCGwLnUh_ZdY6qU'
+//       // 'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     redirect: 'follow', // manual, *follow, error
+//     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//     //body: JSON.stringify(data) // body data type must match "Content-Type" header
+//   });
+//   return response.json(); // parses JSON response into native JavaScript objects
+// }
